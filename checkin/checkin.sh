e602a5f68dcc670f91fcc42e5e3d67fbf965cf29
Javier Cardona <javier@cozybit.com>
2012-02-02 12:45:10 -0800

    Simpler checkin.sh

--- checkin/checkin.sh
+++ checkin/checkin.sh
@@ -1,24 +1,15 @@
 #! /bin/bash
 
-URL='http://o11s-dashboard.appspot.com'
-NETID='testnetwork'
+# Run this script from cron, eg.
+# 1. sudo crontab -e
+# 2. add a line like
+# */5 * * * * /root/o11s-dash/checkin.sh
+# 3. confirm that the Last Seen field for your mesh node is updated
 
-while getopts "l" opt; do
-	case $opt in
-		l)
-		URL='http://localhost:8080'
-		;;
-	esac
-done
+DASHBOARD_URL='http://o11s-dashboard.appspot.com'
+NETID='605_market'
+MACADDR=`ip link show mesh0 | grep 'link/ether' | awk '{ print $2 }'`
 
-# create this network (OK if it already exists)
-curl -s $URL/addnetwork -d "netid=${NETID}"
-
-# create this node (OK if it already exists)
-curl -s $URL/addnode -d "netid=${NETID}&macaddr=00:11:22:33:44:55"
-
-# checkin
-curl -f -s $URL/checkin -d "netid=${NETID}&macaddr=00:11:22:33:44:55"
-[ $? == 0 ] || { echo FAIL; exit -1; }
-
-echo SUCCESS
+wget -O - -d --post-data "netid=${NETID}&macaddr=${MACADDR}" ${DASHBOARD_URL}/checkin
+# OR
+# curl -f -s ${DASHBOARD_URL}/checkin -d "netid=${NETID}&macaddr=00:11:22:33:44:55"
